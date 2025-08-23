cb2bc6dd946ccfb1a0dbc78e0cf890bc13c2bc91
kylewang1999 <k5wang@ucsd.edu>
2025-08-23 16:51:22 -0700

    added nn version

--- scripts/double_integrator.py
+++ scripts/double_integrator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import jax, flax
 from flax.struct import dataclass as flax_dataclass
-from flax.struct import field
+from flax.struct import field, PyTreeNode
 import flax.nnx as nnx
 import jax.numpy as jnp
 import diffrax as dfx
@@ -9,6 +9,7 @@ import jax_dataclasses as jdc
 from typing import Callable
 from functools import partial
 from jax import vmap
+from einops import rearrange
 
 @flax_dataclass
 class CfgRollout:
@@ -17,58 +18,126 @@ class CfgRollout:
     dt: float = 0.01
     
 @flax_dataclass
-class CfgROM:
-    kpsi: float = 1.0
-    ke: float = 1.0
-    kv: float = 1.0
-    lamv: float = 4.0
-    
-@flax_dataclass
 class CfgPD:
     kp: float = 1.0
     kd: float = 1.0
 
-    
-def encode(x: jnp.ndarray) -> tuple[float, float]:
-    y = x[..., 1]
-    z = x[..., 0]
-    return y, z
-
-
-def decode(y: float | jnp.ndarray, z: float | jnp.ndarray) -> jnp.ndarray:
-    return jnp.stack([y, z], axis=-1)
-
-
-def dyn_x(x: jnp.ndarray, u: jnp.ndarray) -> jnp.ndarray:
-    x1dot = x[..., 1]
-    x2dot = u
-    return jnp.stack([x1dot, x2dot], axis=-1)
-
-
-def dyn_yz(y:jnp.array, z:jnp.array, u:jnp.array, komega:float) -> tuple[jnp.array, jnp.array]:
-    ydot = u
-    zdot = y
-    return ydot, zdot
-
-
-def lyap(z: jnp.array, kv: float) -> float:
-    return 0.5 * kv * (z ** 2)
-
-
-def policy_v(y: jnp.ndarray, z: jnp.ndarray, cfg: CfgROM) -> jnp.array:
-    v = -cfg.kpsi * y - cfg.ke * (y + cfg.kpsi*z)
-    return v
-
 
 @flax_dataclass
-class ArgsDiffrax:
-    cfg_rom: CfgROM = field(pytree_node=False, default=CfgROM())
-    encode: Callable = field(pytree_node=False, default=encode)
-    decode: Callable = field(pytree_node=False, default=decode)
-    dyn_x: Callable = field(pytree_node=False, default=dyn_x)
-    dyn_yz: Callable = field(pytree_node=False, default=dyn_yz)
-    lyap: Callable = field(pytree_node=False, default=lyap)
-    policy_v: Callable = field(pytree_node=False, default=policy_v)
+class CfgDIROM:
+    kpsi: float = 1.0
+    ke: float = 1.0
+    kv: float = 1.0
+    lamv: float = 4.0
+
+class DoubleIntegratorROM(PyTreeNode):
+    cfg_rom: CfgDIROM = field(pytree_node=False, default=CfgDIROM())
+    
+
+    def encode(self, x: jnp.ndarray) -> tuple[float, float]:
+        y = x[..., 1]
+        z = x[..., 0]
+        return y, z
+
+    def decode(self, y: float | jnp.ndarray, z: float | jnp.ndarray) -> jnp.ndarray:
+        return jnp.hstack([z, y])
+
+    def dyn_x(self, x: jnp.ndarray, u: jnp.ndarray) -> jnp.ndarray:
+        x1dot = x[..., 1]
+        x2dot = u
+        return jnp.hstack([x1dot, x2dot])
+
+    def dyn_y(self, y:jnp.array, u:jnp.array) -> jnp.array:
+        ydot = u
+        return ydot
+
+    def dyn_z(self, y:jnp.array, z:jnp.array) -> jnp.array:
+        zdot = y
+        return zdot
+    
+    def policy_v(self, y: jnp.ndarray, z: jnp.ndarray) -> jnp.array:
+        v = -self.cfg_rom.kpsi * y - self.cfg_rom.ke * (y + self.cfg_rom.kpsi*z)
+        return v
+
+    def policy_psi(self, z: jnp.ndarray) -> jnp.array:
+        return -self.cfg_rom.kpsi * z
+    
+    def lyap(self, z: jnp.array) -> float:
+        return 0.5 * self.cfg_rom.kv * (z ** 2)
+
+
+
+class NNDoubleIntegratorROM(DoubleIntegratorROM):
+    cfg_rom: CfgDIROM = field(pytree_node=False, default=CfgDIROM())
+    rngs: nnx.Rngs = field(pytree_node=False, default=nnx.Rngs(0))
+    nn_encoder: nnx.Module = field(pytree_node=False, default=nnx.Linear(2,2, use_bias=False, rngs=nnx.Rngs(0)))
+    nn_decoder: nnx.Module = field(pytree_node=False, default=nnx.Linear(2,2, use_bias=False, rngs=nnx.Rngs(0)))
+    nn_fy: nnx.Module = field(pytree_node=False, default=nnx.Linear(1,1, use_bias=False, rngs=nnx.Rngs(0)))
+    nn_gy: nnx.Module = field(pytree_node=False, default=nnx.Linear(1,1, use_bias=True, rngs=nnx.Rngs(0)))
+    nn_fz: nnx.Module = field(pytree_node=False, default=nnx.Linear(2,1, use_bias=False, rngs=nnx.Rngs(0)))
+    nn_psi: nnx.Module = field(pytree_node=False, default=nnx.Linear(1,1, use_bias=False, rngs=nnx.Rngs(0)))
+    
+    
+    def hardcode_nn_params(self, ):
+        """Explicitly set parameters for all neural network modules.
+        
+        Args:
+            params: Dictionary containing parameter arrays for each module.
+                   Keys should match the module names (e.g., 'nn_decoder', 'nn_gy', etc.)
+        """
+
+        self.nn_encoder.kernel.value = jnp.array([[0.0, 1.0], [1.0, 0.0]])
+        self.nn_decoder.kernel.value = jnp.array([[0.0, 1.0], [1.0, 0.0]])
+        
+        self.nn_fy.kernel.value = jnp.array([[0.0]])
+        self.nn_gy.kernel.value = jnp.array([[0.0]])
+        self.nn_gy.bias.value = jnp.array([1.0])
+        
+        self.nn_fz.kernel.value = jnp.array([[1.0], [0.0]])
+        self.nn_psi.kernel.value = jnp.array([[self.cfg_rom.kpsi]])
+    
+    
+    def encode(self, x: jnp.ndarray) -> tuple[float, float]:
+        yz = self.nn_encoder(x)
+        y, z = yz[..., 0], yz[..., 1]
+        return y, z
+    
+    def decode(self, y: jnp.ndarray, z: jnp.ndarray) -> jnp.ndarray:
+        return self.nn_decoder(jnp.stack([y, z], axis=-1))
+    
+    def dyn_x(self, x: jnp.ndarray, u: jnp.ndarray) -> jnp.ndarray:
+        return super().dyn_x(x, u)
+    
+    def dyn_y(self, y: jnp.ndarray, u: jnp.ndarray) -> jnp.ndarray:
+        return self.nn_fy(y) + self.nn_gy(y) * u
+    
+    def dyn_z(self, y: jnp.ndarray, z: jnp.ndarray) -> jnp.ndarray:
+        return self.nn_fz(jnp.hstack([y, z]))
+    
+    def policy_psi(self, z: jnp.ndarray) -> jnp.ndarray:
+        return -self.nn_psi(z)
+    
+    def dist_to_manifold(self, y: jnp.ndarray, z: jnp.ndarray) -> jnp.ndarray:
+        return y - self.policy_psi(z)
+    
+    def policy_v(self, y: jnp.ndarray, z: jnp.ndarray) -> jnp.ndarray:   
+        y = jnp.atleast_1d(y)
+        z = jnp.atleast_1d(z)
+        
+        gy = self.nn_gy(y)
+        g_inv = 1.0 / gy
+        zdot = self.dyn_z(y, z)
+        
+        _, dpsidz_times_omega = jax.jvp(self.policy_psi, (z,), (zdot,))
+        fy = self.nn_fy(y)
+        return g_inv * (dpsidz_times_omega
+                        - self.cfg_rom.ke * self.dist_to_manifold(y, z)
+                        - fy)
+    
+    def lyap(self, z: jnp.array) -> float:
+        return super().lyap(z)
+
+
 
 @flax_dataclass
 class IntegratorOutput:
@@ -77,7 +146,7 @@ class IntegratorOutput:
     
     
 @flax_dataclass
-class RetRollout:
+class IntegratorDebugOutput:
     xs: jnp.ndarray
     ys: jnp.ndarray
     zs: jnp.ndarray
@@ -85,12 +154,13 @@ class RetRollout:
     vs: jnp.ndarray
     es: jnp.ndarray
     ts: jnp.ndarray
+    lyaps: jnp.ndarray
 
 
-class Integrator(flax.struct.PyTreeNode):
+class Integrator(PyTreeNode):
     solver: Callable
     ts: jnp.array = field(pytree_node=False)
-    args_dfx: ArgsDiffrax = field(pytree_node=False, default=ArgsDiffrax())
+    rom: DoubleIntegratorROM = field(pytree_node=False, default=DoubleIntegratorROM())
 
     @property
     def dt0(self) -> float: return self.ts[1] - self.ts[0]
@@ -108,54 +178,13 @@ class Integrator(flax.struct.PyTreeNode):
         }
 
 
-    def apply(self, x0):
-        
-        int_out = IntegratorOutput(
-            xs=jnp.zeros((self.n_steps+1, 2)),
-            us=jnp.zeros((self.n_steps, 1))
-        )
-        int_out = int_out.replace(xs=int_out.xs.at[0].set(x0))
-        
-        def body(i, carry):
-            int_out = carry
-            x_curr = int_out.xs[i]
-            y, z = self.args_dfx.encode(x_curr)
-            u = self.args_dfx.policy_v(y, z, self.args_dfx.cfg_rom)
-            u = jnp.atleast_1d(u)
-
-            def _term(t, x, args):
-                # args is (params, u), use self.args_dfx for dynamics function
-                # return self.args_dfx.dyn_x(x, args[1].squeeze())
-                return self.args_dfx.dyn_x(x, u.squeeze())
-            
-            sol = self.solver(
-                dfx.ODETerm(_term),
-                dt0=self.dt0,
-                t0=self.ts[i],
-                t1=self.ts[i + 1],
-                y0=x_curr,
-                # args=(params, u)
-                args=None,
-            )
-            
-            int_out = int_out.replace(
-                xs=int_out.xs.at[i + 1].set(sol.ys[-1]),
-                us=int_out.us.at[i].set(u)
-            )
-
-            return int_out
-        
-        int_out_final = jax.lax.fori_loop(
-            lower=0, 
-            upper=self.n_steps, 
-            body_fun=body, 
-            init_val=int_out
-        )
-        int_out_final = int_out
-        return int_out_final
-    
-
-    def apply_batch(self, x0s):
+    def apply(self, x0s):
+        ''' Integrate the dynamics from self.ts[0] to self.ts[-1] with initial condition x0s.
+        Input:
+            x0s: (B, 2)
+        Output:
+            int_out: IntegratorOutput
+        '''
         
         int_out = IntegratorOutput(
             xs=jnp.zeros((x0s.shape[0], self.n_steps+1, 2)),  # (B, T+1, 2)
@@ -166,13 +195,16 @@ class Integrator(flax.struct.PyTreeNode):
         def body(i, carry):
             int_out = carry
             x_curr = int_out.xs[:,i]
-            y, z = vmap(self.args_dfx.encode, in_axes=0)(x_curr)
-            u = vmap(self.args_dfx.policy_v, in_axes=(0, 0, None))(y, z, self.args_dfx.cfg_rom)
+            y, z = vmap(self.rom.encode, in_axes=0)(x_curr)
+            u = vmap(self.rom.policy_v, in_axes=(0, 0))(y, z)
+            
+            if u.ndim == 1:
+                u = u[:,None]
 
             def _term(t, x, args):
                 # args is (params, u), use self.args_dfx for dynamics function
                 # return self.args_dfx.dyn_x(x, args[1].squeeze())
-                return vmap(self.args_dfx.dyn_x, in_axes=(0, 0))(x, u)
+                return vmap(self.rom.dyn_x, in_axes=(0, 0))(x, u)
             
             sol = self.solver(
                 dfx.ODETerm(_term),
@@ -186,7 +218,7 @@ class Integrator(flax.struct.PyTreeNode):
             
             int_out = int_out.replace(
                 xs=int_out.xs.at[:,i + 1].set(sol.ys[-1]),
-                us=int_out.us.at[:,i].set(u[:,None])
+                us=int_out.us.at[:,i].set(u)
             )
 
             return int_out
@@ -197,48 +229,36 @@ class Integrator(flax.struct.PyTreeNode):
             body_fun=body, 
             init_val=int_out
         )
-        int_out_final = int_out
         return int_out_final
     
-
- 
-def rollout(x0: jnp.ndarray, cfg_rollout: CfgRollout, cfg_rom: CfgROM) -> RetRollout:
-
-    ts = jnp.arange(cfg_rollout.t0, cfg_rollout.t1 + cfg_rollout.dt, cfg_rollout.dt)
-
-    def _term(t, x, args: ArgsDiffrax):
-        y, z = encode(x)
-        v = args.policy_v(y, z, args.cfg_rom)
-        u = v
-        return dyn_x(x, u)
-
-    term   = dfx.ODETerm(_term)
-    solver = dfx.Tsit5()
-    saveat = dfx.SaveAt(ts=ts)
-
-    sol = dfx.diffeqsolve(
-        term, solver,
-        t0=cfg_rollout.t0, t1=cfg_rollout.t1,
-        dt0=cfg_rollout.dt,
-        y0=jnp.asarray(x0, dtype=jnp.float32),
-        saveat=saveat,
-        args=ArgsDiffrax(),
-    )
-
-    xs = sol.ys                              # (T, 2)
-    ys, zs = xs[:, 1], xs[:, 0]
-    vs = policy_v(ys, zs, cfg_rom)           # (T,)
-    us = vs                                  # u ≡ v
-    es = ys - cfg_rom.kpsi * zs
-
-    return RetRollout(xs, ys, zs, us, vs, es, ts)
+    
+    def post_apply(self, int_out: IntegratorOutput) -> IntegratorDebugOutput:
+        ''' Augment IntegratorOutput with debug information. '''
+        xs, us = int_out.xs, int_out.us # (B, T+1, 2), (B, T, 1)
+        B = xs.shape[0]
+        ys, zs = vmap(self.rom.encode, in_axes=0)(rearrange(xs, 'b t d -> (b t) d'))
+        ys = ys[..., None]
+        zs = zs[..., None]
+        vs = vmap(self.rom.policy_v, in_axes=(0, 0))(ys, zs)
+        psis = vmap(self.rom.policy_psi, in_axes=0)(zs)
+        es = jnp.abs(ys - psis)
+        lyaps = vmap(self.rom.lyap, in_axes=0)(zs)
+        
+        xs = xs
+        ys = rearrange(ys, '(b t) d -> b t d', b=B)
+        zs = rearrange(zs, '(b t) d -> b t d', b=B)
+        us = us
+        vs = rearrange(vs, '(b t) d -> b t d', b=B)
+        es = rearrange(es, '(b t) d -> b t d', b=B)
+        lyaps = rearrange(lyaps, '(b t) d -> b t d', b=B)
+        
+        return IntegratorDebugOutput(xs, ys, zs, us, vs, es, self.ts, lyaps)
+    
 
 
 if __name__ == "__main__":
-    cfg_rom = CfgROM()
+    cfg_rom = CfgDIROM()
     cfg_rollout = CfgRollout()
-    x0 = jnp.array([1.0, 1.0])
-    ret = rollout(x0, cfg_rollout, cfg_rom)
 
     rng = jax.random.PRNGKey(42)
     ts = jnp.arange(cfg_rollout.t0, cfg_rollout.t1 + cfg_rollout.dt, cfg_rollout.dt)
@@ -247,13 +267,8 @@ if __name__ == "__main__":
     integrator = Integrator(
         solver=partial(dfx.diffeqsolve, solver=dfx.Tsit5()),
         ts=ts,
-        args_dfx=ArgsDiffrax(cfg_rom=cfg_rom),
+        rom=DoubleIntegratorROM(cfg_rom=cfg_rom),
     )
 
-    # single
-    out1 = integrator.apply(x0s_batch[0])
-    print("Single integrator shape:", out1.xs.shape)
-
-    # batched (vmap over x0), self is static
-    out2 = integrator.apply_batch(x0s_batch)
-    print("Batched integrator shape:", out2.xs.shape)   # (B, T, 2)
+    out = integrator.apply(x0s_batch)
+    print("Batched integrator shape:", out.xs.shape)   # (B, T, 2)
